925b7a0cc5abb4fdc0ba798f0f7de02ab03ede09
vmeaning <45858352+vmeaning@users.noreply.github.com>
2020-05-23 11:37:28 +0300

    Update cards.js

--- cards.js
+++ cards.js
@@ -2,15 +2,17 @@
 
 //prototype
 let aCard = {
-    suit: undefined,
-    value: undefined
+    _suit: undefined,
+    _value: undefined,
+    _color: undefined
 }
 
 //constructor
 function TheCard(suit, value){
     this.__proto__ = aCard;
-    this.suit = suit.toUpperCase();
-    this.value = value;
-    this.screenName = this.suit + this.value;
+    this._suit = suit.toUpperCase();
+    this._value = value;
+    this._screenName = this._suit + this._value;
     // TODO: сделать проверку валидности входных данных. Объект не должен создаваться, если данные не валидные
+    // TODO create getters for screenName, suit and value
 }
